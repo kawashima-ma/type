74f8f95539454dc181a71d180e964a68fe5b45ae
kawashima.mayumi <kawashima.mayumi@m.alhinc.jp>
2019-11-08 18:03:17 +0900

    1803

--- src/main/java/jp/co/type/Utils/CipherUtil.java
+++ src/main/java/jp/co/type/Utils/CipherUtil.java
@@ -1,5 +1,19 @@
 package jp.co.type.Utils;
 
-public class CipherUtil {
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
+import org.springframework.util.Base64Utils;
+
+public class CipherUtil {
+	public static String encrypt(String target) {
+
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			md.update(target.getBytes());
+			return Base64Utils.encodeToString(md.digest());
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
--- src/main/java/jp/co/type/controller/LoginController.java
+++ src/main/java/jp/co/type/controller/LoginController.java
@@ -1,8 +1,47 @@
 package jp.co.type.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jp.co.type.dto.UserDto;
+import jp.co.type.form.LoginForm;
+import jp.co.type.service.LoginService;
 
 @Controller
 public class LoginController {
+	@Autowired
+	private LoginService loginService;
+	@Autowired
+	private HttpSession session;
 
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String doGet(@ModelAttribute LoginForm loginForm, Model model) {
+		model.addAttribute("loginForm", loginForm);
+		return "login";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String doPost(@ModelAttribute LoginForm loginForm, Model model, BindingResult result) {
+
+		if(result.hasErrors()){
+			model.addAttribute("loginForm", loginForm);
+			model.addAttribute("errormessages", "ログインに失敗しました");
+        		return "login";
+		}
+
+		UserDto user = loginService.login(loginForm.getLogin_id(), loginForm.getPassword());
+		if(user == null) {
+			model.addAttribute("errormessages", "ログインに失敗しました");
+			return "login";
+		}
+		session.setAttribute("loginUser",user);
+		return "redirect:showMessage";
+	}
 }

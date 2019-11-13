9a0a5c49597ca2cca92bdfeb85aebea0b7052be7
yoshida.tatsuya <yoshida.tatsuya@m.alhinc.jp>
2019-11-13 15:25:48 +0900

    commit

---
+++ src/main/java/jp/co/type/controller/UserPastResultController.java
@@ -0,0 +1,43 @@
+package jp.co.type.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jp.co.type.dto.UserResultDto;
+import jp.co.type.service.UserResultService;
+
+@Controller
+public class UserPastResultController {
+	@Autowired
+	private UserResultService resultService;
+//	@Autowired
+//	private EditFormFactory editFormFactory;
+//	@Autowired
+//	private UserDtoFactory userDtoFactory;
+	@Autowired
+	private HttpSession session;
+
+	@RequestMapping(value = "/pastuserresults", method = RequestMethod.GET)
+	public String edit(Model model) {
+
+
+
+//		UserDto loginUser = (UserDto) session.getAttribute("loginUser");
+//		// 編集対象のユーザー情報を取得する
+//		UserResultDto UserResult = resultService.getEditUser(loginUser.getId());
+
+		UserResultDto userResult = resultService.getUserResult(1);
+
+System.out.println(userResult.getDrivescore());
+
+//		model.addAttribute("editForm", editFormFactory.create(editUser));
+//		model.addAttribute("isShowPulldown", isShowPulldown(managementForm.getIdAsInteger(),
+//				((UserDto) session.getAttribute("loginUser")).getId()));
+		return "pastuserresults";
+	}
+}
---
+++ src/main/java/jp/co/type/entity/UserResult.java
@@ -0,0 +1,92 @@
+package jp.co.type.entity;
+
+import java.util.Date;
+
+public class UserResult {
+	private Integer id;
+	private String login_id;
+	private String name;
+	private Integer result_type;
+	private Integer drivescore;
+	private Integer analyzescore;
+	private Integer createscore;
+	private Integer volunteerscore;
+	private Date createdAt;
+	private Date updatedAt;
+	private String type_name;
+	private Integer result_id;
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getLogin_id() {
+		return login_id;
+	}
+	public void setLogin_id(String login_id) {
+		this.login_id = login_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getResult_type() {
+		return result_type;
+	}
+	public void setResult_type(Integer result_type) {
+		this.result_type = result_type;
+	}
+	public Integer getDrivescore() {
+		return drivescore;
+	}
+	public void setDrivescore(Integer drivescore) {
+		this.drivescore = drivescore;
+	}
+	public Integer getAnalyzescore() {
+		return analyzescore;
+	}
+	public void setAnalyzescore(Integer analyzescore) {
+		this.analyzescore = analyzescore;
+	}
+	public Integer getCreatescore() {
+		return createscore;
+	}
+	public void setCreatescore(Integer createscore) {
+		this.createscore = createscore;
+	}
+	public Integer getVolunteerscore() {
+		return volunteerscore;
+	}
+	public void setVolunteerscore(Integer volunteerscore) {
+		this.volunteerscore = volunteerscore;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	public String getType_name() {
+		return type_name;
+	}
+	public void setType_name(String type_name) {
+		this.type_name = type_name;
+	}
+	public Integer getResult_id() {
+		return result_id;
+	}
+
+	public void setResult_id(Integer result_id) {
+		this.result_id = result_id;
+	}
+}

945348343b24767069d12989c705c764b22d5f00
yano.erina <yano.erina@m.alhinc.jp>
2019-11-13 15:26:05 +0900

    first commit

--- src/main/java/jp/co/type/form/SignupForm.java
+++ src/main/java/jp/co/type/form/SignupForm.java
@@ -1,23 +1,32 @@
 package jp.co.type.form;
 
 import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.GroupSequence;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class SignupForm {
-	@NotEmpty(message = "ログインIDを入力してください")
-	@Size(min = 6, max = 20,message = "ログインIDを、6~20文字以下で入力して下さい")
-	@Pattern(regexp = "[a-zA-Z0-9]+")
+	public interface Group1 {}
+	public interface Group2 {}
+	public interface Group3 {}
+
+	@GroupSequence({Group1.class,Group2.class,Group3.class})
+	public interface All {}
+
+	@NotEmpty(message = "ログインIDを入力してください",groups = Group1.class)
+	@Size(min = 6, max = 20,message = "ログインIDを、6~20文字以下で入力して下さい",groups = Group2.class)
+	@Pattern(regexp = "[a-zA-Z0-9]+",message = "ログインIDを半角英数字で入力して下さい",groups = Group3.class)
 
 	private String login_id;
 	@NotEmpty(message = "名前を入力してください")
 	@Size(min = 6, max = 20,message = "名前を、6~20文字以下で入力して下さい")
-	@Pattern(regexp = "[a-zA-Z0-9]+")
+	@Pattern(regexp = "[a-zA-Z0-9]+",message = "名前を半角英数字で入力して下さい")
 
 	private String name;
 	@NotEmpty(message = "パスワードを入力してください")
 	@Size(min = 6, max = 20,message = "パスワードを、6~20文字以下で入力して下さい")
-	@Pattern(regexp = "[a-zA-Z0-9]+")
+	@Pattern(regexp = "[a-zA-Z0-9]+",message = "パスワードを半角英数字で入力して下さい")
 	private String password;
 
 

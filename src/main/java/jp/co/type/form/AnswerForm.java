00131bd6e87dc77c5835c7fccb7225771febe048
ono_yuta <ono.yuta@m.alhinc.jp>
2019-11-13 15:41:17 +0900

    20191113 ono

---
+++ src/main/java/jp/co/type/form/AnswerForm.java
@@ -0,0 +1,36 @@
+package jp.co.type.form;
+
+public class AnswerForm {
+	private String[] drive_ans;
+	private String[] analyze_ans;
+	private String[] create_ans;
+	private String[] volunteer_ans;
+
+
+	public String[] getDrive_ans() {
+		return drive_ans;
+	}
+	public void setDrive_ans(String[] drive_ans) {
+		this.drive_ans = drive_ans;
+	}
+	public String[] getAnalyze_ans() {
+		return analyze_ans;
+	}
+	public void setAnalyze_ans(String[] analyze_ans) {
+		this.analyze_ans = analyze_ans;
+	}
+	public String[] getCreate_ans() {
+		return create_ans;
+	}
+	public void setCreate_ans(String[] create_ans) {
+		this.create_ans = create_ans;
+	}
+	public String[] getVolunteer_ans() {
+		return volunteer_ans;
+	}
+	public void setVolunteer_ans(String[] volunteer_ans) {
+		this.volunteer_ans = volunteer_ans;
+	}
+
+
+}

c6b65d20bf27d1ecaa08db202114c989aeeea2f9
yoshida.tatsuya <yoshida.tatsuya@m.alhinc.jp>
2019-11-15 10:06:05 +0900

    commit

--- src/main/java/jp/co/type/form/AnswerForm.java
+++ src/main/java/jp/co/type/form/AnswerForm.java
@@ -3,35 +3,22 @@ package jp.co.type.form;
 import java.util.List;
 
 public class AnswerForm {
-	private List<String> drive_ans;
-	private List<String> analyze_ans;
-	private List<String> create_ans;
-	private List<String> volunteer_ans;
+	private List<String> point2lists;
+	private List<String> point1lists;
+	public List<String> getPoint2lists() {
+		return point2lists;
+	}
+	public void setPoint2lists(List<String> point2lists) {
+		this.point2lists = point2lists;
+	}
+	public List<String> getPoint1lists() {
+		return point1lists;
+	}
+	public void setPoint1lists(List<String> point1lists) {
+		this.point1lists = point1lists;
+	}
 
-	public List<String> getDrive_ans() {
-		return drive_ans;
-	}
-	public void setDrive_ans(List<String> drive_ans) {
-		this.drive_ans = drive_ans;
-	}
-	public List<String> getAnalyze_ans() {
-		return analyze_ans;
-	}
-	public void setAnalyze_ans(List<String> analyze_ans) {
-		this.analyze_ans = analyze_ans;
-	}
-	public List<String> getCreate_ans() {
-		return create_ans;
-	}
-	public void setCreate_ans(List<String> create_ans) {
-		this.create_ans = create_ans;
-	}
-	public List<String> getVolunteer_ans() {
-		return volunteer_ans;
-	}
-	public void setVolunteer_ans(List<String> volunteer_ans) {
-		this.volunteer_ans = volunteer_ans;
-	}
+
 
 
 

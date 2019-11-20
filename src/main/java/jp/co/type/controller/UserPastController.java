8014afd3e986fe1499f2123a47dbf73716cf19bf
ono_yuta <ono.yuta@m.alhinc.jp>
2019-11-20 09:44:57 +0900

    20191120 ono commit

--- src/main/java/jp/co/type/controller/UserPastController.java
+++ src/main/java/jp/co/type/controller/UserPastController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jp.co.type.dto.UserResultDto;
 import jp.co.type.service.UserResultService;
@@ -29,17 +30,15 @@ public class UserPastController {
 
 
 	@RequestMapping(value = "/userPast", method = RequestMethod.GET)
-	public String past(Model model) {
+	public String past(Model model, @RequestParam("user_id") int num) {
 
-
-Integer num = 1;
 		List<UserResultDto> userpastResults = resultService.getUserResultAll(num);
 
 		model.addAttribute("userpastResults",userpastResults);
 //		model.addAttribute("editForm", editFormFactory.create(editUser));
 //		model.addAttribute("isShowPulldown", isShowPulldown(managementForm.getIdAsInteger(),
 //				((UserDto) session.getAttribute("loginUser")).getId()));
-		return "past";
+		return "/userPast";
 	}
 
 }
